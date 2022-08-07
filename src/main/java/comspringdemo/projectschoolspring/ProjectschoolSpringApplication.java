37a9e01caaacf305cfef9a7dd2494fc1c42d3e27
Bianca Ciorobea <ciorobea.b@northeastern.edu>
2022-08-07 09:13:42 -0400

    spring initializr project demo

---
+++ src/main/java/comspringdemo/projectschoolspring/ProjectschoolSpringApplication.java
@@ -0,0 +1,13 @@
+package comspringdemo.projectschoolspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectschoolSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectschoolSpringApplication.class, args);
+  }
+
+}
